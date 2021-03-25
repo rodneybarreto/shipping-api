f8b442a23994c81f566159c912477c05d1ce860a
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-25 12:46:55 -0300

    Add dotenv package

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 
@@ -10,7 +12,7 @@ app.use('*', (req, res) => {
   res.sendStatus(400)
 })
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Shipping-API is running on port: ${PORT}`)
 })
