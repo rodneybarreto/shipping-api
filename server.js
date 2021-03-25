3bd51ed0e22bdbb72bce9f813fb563372bb97083
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-25 12:14:12 -0300

    Send status 400 to others endpoints

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const PORT = process.env.PORT || 3001
 
+const app = express()
 require('./src/routes/index')(app)
 
-app
-  .use(cors())
-  .listen(PORT, () => {
-    console.log(`Shipping Server is running on port: ${PORT}`)
-  })
+app.use(cors())
 
-app.get('/health', (req, res) => {
-  res.json({health: 'Shipping API is running!'})
+app.use('*', (req, res) => {
+  res.sendStatus(400)
+})
+
+const PORT = process.env.port || 3001
+app.listen(PORT, () => {
+  console.log(`Shipping-API is running on port: ${PORT}`)
 })
