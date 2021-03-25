ff9be819981ad4e5802abbfd2282f5777f9eb71d
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-25 12:14:48 -0300

    Edit

--- src/controllers/ShippingController.js
+++ src/controllers/ShippingController.js
@@ -1,5 +1,5 @@
 exports.getByZipcode = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   
   const rate = parseFloat((Math.random() * 100 + 1).toFixed(2))
   res.status(200).json({zipcode: req.params.zipcode, shippingRate: rate})
