4e106592e284627f1d2361ccb9a99838bde02371
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-27 15:08:19 -0300

    Logging shipping request

--- src/controllers/ShippingController.js
+++ src/controllers/ShippingController.js
@@ -2,5 +2,8 @@ exports.getByZipcode = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   
   const rate = parseFloat((Math.random() * 100 + 1).toFixed(2))
-  res.status(200).json({zipcode: req.params.zipcode, shippingRate: rate})
+  const shipping = { zipcode: req.params.zipcode, shippingRate: rate }
+  
+  console.log(shipping)
+  res.status(200).json(shipping)
 }
