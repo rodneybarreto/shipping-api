58d239b2422a54628957ec9decc45a96907601b2
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-29 14:03:27 -0300

    Edit dockerignore

--- docker.build-image.sh
+++ docker.build-image.sh
@@ -4,7 +4,7 @@
 docker build . -t rodneybarreto/minipets_shipping-api:1.0.0
 
 ## Push to docker hub
-#docker push rodneybarreto/minipets_shipping-api:1.0.0
+docker push rodneybarreto/minipets_shipping-api:1.0.0
 
 ## Run container
 #docker run --name shipping-api --network=host -p 8082:8082 rodneybarreto/minipets_shipping-api:1.0.0
